93e6d98256e3bef43894d37f6a9fccd8e9f1556f
BernardIT001 <78587719+BernardIT001@users.noreply.github.com>
2024-01-26 10:53:41 +0000

    2nd commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    member.style.display = "none";
+    var skills = document.getElementById("skills");
+    skills.style.display = "block";
+    var about = document.getElementById("about");
+    about.style.display = "none";
+    var contact = document.getElementById("contact");
+    contact.style.display = "none";
+    var home = document.getElementById("home");
+    home.style.display = "none";
+  }
